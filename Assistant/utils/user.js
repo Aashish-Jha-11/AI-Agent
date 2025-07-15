87c08a504a38e02a67ea67425f16eedd98f2e81d
Aashish Jha <aashishjha1107@gmail.com>
2025-07-16 00:01:56 +0530

    user.js

--- Assistant/utils/user.js
+++ Assistant/utils/user.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, default: "user", enum: ["user", "moderator", "admin"] },
+  createdAt: { type: Date, default: Date.now },
 });
 
 export default mongoose.model("User", userSchema);
